5a1033c3676fbe2b1b75b6b59ddf11c4c9b0d6ca
Chris Tessmer <chris.tessmer@onyxpoint.com>
2020-04-06 12:41:25 -0400

    Remove debug cruft

--- dist/puppetsync/functions/ensure_jira_subtask_for_each_repo.pp
+++ dist/puppetsync/functions/ensure_jira_subtask_for_each_repo.pp
@@ -5,10 +5,8 @@ function puppetsync::ensure_jira_subtask_for_each_repo(
   Sensitive[String[1]] $jira_token             = Sensitive(system::env('JIRA_API_TOKEN')),
   Array[Stdlib::Absolutepath] $extra_gem_paths = ["${pwd}/gems"]
 ) {
-  warning('1')
   $repos.each |$target| {
     assert_type( Hash, $puppetsync_config['jira'])
-    warning("2: ${target.name}")
     $assignee = $puppetsync_config['jira']['subtask_assignee'].empty ? {
       false   =>  $puppetsync_config['jira']['subtask_assignee'],
       default => undef,
@@ -21,39 +19,6 @@ function puppetsync::ensure_jira_subtask_for_each_repo(
       false   =>  $puppetsync_config['jira']['subtask_story_points'],
       default => undef,
     }
-    warning("3: ${target.name}")
-
-    warning("-------------------puppetsync_config:\n${puppetsync_config}\n-------------------")
-    warning("-------------------target.vars:\n${target.vars}\n-------------------")
-    warning("-------------------description:\n${description}\n-------------------")
-    warning("-------------------story_points:\n${story_points}\n-------------------")
-    warning("-------------------assignee:\n${assignee}\n-------------------")
-    warning("-------------------jira_username:\n${jira_username}\n-------------------")
-    warning("-------------------jira_token.unwrap:\n${jira_token.unwrap}\n-------------------")
-    warning("-------------------extra_gem_paths:\n${extra_gem_paths}\n-------------------")
-   
-    warning("\$results = run_task(
-      'puppetsync::ensure_jira_subtask',
-      $target,
-      \"Ensure a Jira subtask under ${puppetsync_config['jira']['parent_issue']} exists\",
-      {
-        'component_name'       => ${target.vars['mod_data']['repo_name']},
-        'parent_issue'         => ${puppetsync_config['jira']['parent_issue']},
-        'project'              => ${puppetsync_config['jira']['project']},
-        'subtask_title'        => ${puppetsync_config['jira']['subtask_title']},
-        'subtask_description'  => ${description},
-        'subtask_story_points' => ${story_points},
-        'subtask_assignee'     => ${assignee},
-        'jira_site'            => ${puppetsync_config['jira']['jira_site']},
-        'jira_username'        => ${jira_username},
-        'jira_token'           => ${jira_token.unwrap},
-        'extra_gem_paths'      => ${extra_gem_paths},
-        '_catch_errors'        => true,
-      }
-    )"
-    )
-
-
 
     $results = run_task(
       'puppetsync::ensure_jira_subtask',
@@ -75,7 +40,6 @@ function puppetsync::ensure_jira_subtask_for_each_repo(
       }
     )
 
-    warning("4: ${target.name}")
     unless $results.ok { fail("Running puppetsync::ensure_jira_subtask failed on ${target.name}") }
 
     $subtask_key = $results.first.value['subtask_key']
