ca1688efefffd6daf509408c8a5f9651e32a98b7
Chris Tessmer <chris.tessmer@onyxpoint.com>
2020-04-07 13:25:57 -0400

    Remove old puppetsync files

--- scripts/not_a_simp_module.rb
+++
@@ -1,7 +0,0 @@
-require 'puppetsync/script_helpers.rb'
-include Puppetsync::ScriptHelpers
-
-_data = Facter.value(:module_metadata)
-fail 'FAIL: MUST be a Puppet module! (no metadata.json)' unless _data
-fail "FAIL: MUST be a `simp` module! (forge org: '#{_data['forge_org']}')" unless _data['forge_org'] == 'simp'
-fail 'FAIL: MUST not be simp-compliance_markup!' if _data['name'] == 'simp-compliance_markup'
