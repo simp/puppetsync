e45ea2217d83b222e5994dc70afc2fb5d0c5ccf4
Chris Tessmer <chris.tessmer@onyxpoint.com>
2020-04-09 02:06:51 -0400

    Better JSON array parsing from STDIN

--- dist/puppetsync/tasks/install_gems.sh
+++ dist/puppetsync/tasks/install_gems.sh
@@ -9,9 +9,9 @@
 [ -z "$PT_path" ] && { echo '=== $PT_path is empty'; env | grep -E '^(PT_|_)'; exit 1; }
 [ -z "$PT_gems" ] && { echo '=== $PT_gems is empty'; env | grep -E '^(PT_|_)'; exit 1; }
 [ -d "$(dirname "$PT_path")" ] || { echo "=== Directory $(dirname "$PT_path") not found"; env | grep -E '^(PT_|_)'; exit 2; }
-
-gems=($(echo "$PT_gems" |  sed -e 's/[", \[\]]*/ /g'))
-GEM_HOME="$PT_path" /opt/puppetlabs/bolt/bin/gem install --no-document jira-ruby octokit && echo "== PT_path='$PT_path' gems(${#gems[@]})='${gems[@]}'"
+echo "== PT_path='$PT_path' PT_gems='${PT_gems}' gems(${#gems[@]})='${gems[@]}'"
+IFS=$'\n' read -rd '' -a gems < <(echo $PT_gems | /opt/puppetlabs/bolt/bin/ruby -r json -ne 'puts JSON.parse $_')
+GEM_HOME="$PT_path" /opt/puppetlabs/bolt/bin/gem install --no-document "${gems[@]}" && echo "== PT_path='$PT_path' gems(${#gems[@]})='${gems[@]}'"
 
 
 
