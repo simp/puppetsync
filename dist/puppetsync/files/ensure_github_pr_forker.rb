f780c62b7029427cb46ad00f99d1fab20c662cd9
Chris Tessmer <chris.tessmer@onyxpoint.com>
2020-04-09 02:10:04 +0000

    BROKEN SAFETY SAVE: approve/merge PR

--- dist/puppetsync/files/ensure_github_pr_forker.rb
+++ dist/puppetsync/files/ensure_github_pr_forker.rb
@@ -11,10 +11,10 @@ class GitHubPRForker
   end
 
   # return the user's fork of the upstream repo, or nil if there isn't one
-  def forked_user_repo(upstream_reponame)
+  def forked_user_repo(upstream_reponame, user=@user)
     upstream_repo =  @client.repo upstream_reponame
     forks = @client.forks(upstream_repo.full_name)
-    forks = forks.select{|x| x[:owner][:login] == @user }
+    forks = forks.select{|x| x[:owner][:login] == user }
     forks.empty? ? nil : forks.first
   end
 
@@ -31,9 +31,10 @@ class GitHubPRForker
 
   # return array of prs that already exist for this chang
   def existing_prs(upstream_reponame, opts)
-    repo_fork = forked_user_repo(upstream_reponame)
+    user = opts[:user] || @user
+    repo_fork = forked_user_repo(upstream_reponame, user)
     @client.pull_requests( upstream_reponame ).select do |pr|
-      pr.user.login == @user && \
+      pr.user.login == user && \
       pr.head.repo.full_name == repo_fork.full_name && \
       pr.head.ref == opts[:fork_branch] && \
       pr.base.ref == opts[:target_branch]
@@ -69,31 +70,81 @@ class GitHubPRForker
     warn( "--- PR ##{pr.number} already exists for: #{opts[:fork_branch]} -> #{upstream_reponame}:#{opts[:target_branch]}" )
     return pr
   end
+
+  def approve_pr(upstream_reponame, opts )
+    existing_prs_for_this_change = existing_prs(upstream_reponame, opts)
+    # TODO: Check for existing approvals from us, too
+    raise 'No PR exists to approve' if existing_prs_for_this_change.empty?
+    raise 'More than one PR already exists for this change' if existing_prs_for_this_change.size > 1
+    pr = existing_prs_for_this_change.first
+    review_opts = { event: opts[:approval_type], body: opts[:approval_message] || nil }
+    review = @client.create_pull_request_review( upstream_reponame, pr.number, review_opts )
+require 'pry'; binding.pry
+  end
+
+  def merge_pr(upstream_reponame, opts )
+    existing_prs_for_this_change = existing_prs(upstream_reponame, opts)
+    raise 'No PR exists to approve' if existing_prs_for_this_change.empty?
+    raise 'More than one PR already exists for this change' if existing_prs_for_this_change.size > 1
+    pr = existing_prs_for_this_change.first
+require 'pry'; binding.pry
+
+  end
+
 end
 
 if __FILE__ == $0
 
   forker = GitHubPRForker.new( ENV['GITHUB_USER'], ENV['GITHUB_API_TOKEN'])
-  fail( 'set ENV var UPSTREAM_REPO (ex: \'UPSTREAM_REPO=simp/pupmod-simp-aide\')' ) unless ENV['UPSTREAM_REPO']
-  upstream_reponame = ENV['UPSTREAM_REPO'] || 'simp/pupmod-simp-aide'
+  fail( 'set ENV var UPSTREAM_REPO (ex: \'UPSTREAM_REPO=simp/pupmod-simp-at\')' ) unless ENV['UPSTREAM_REPO']
+  upstream_reponame = ENV['UPSTREAM_REPO'] || 'simp/pupmod-simp-at'
   repo_fork = forker.ensure_fork(upstream_reponame)
+  ####opts = {
+  ####  target_branch: 'master',
+  ####  fork_branch:   'SIMP-7035',
+  ####  commit_message: (<<~COMMIT_MESSAGE
+  ####    (SIMP-7035) Update to new Travis CI pipeline
+
+  ####    This patch updates the Travis Pipeline to a static, standardized format
+  ####    that uses project variables for secrets. It includes an optional
+  ####    diagnostic mode to test the project's variables against their respective
+  ####    deployment APIs (GitHub and Puppet Forge).
+
+  ####    SIMP-7035 #comment Update to latest pipeline in pupmod-simp-at
+  ####    SIMP-7617 #close
+  ####    COMMIT_MESSAGE
+  ####  )
+  ####}
+
   opts = {
     target_branch: 'master',
     fork_branch:   'SIMP-7035',
-    commit_message: (<<~COMMIT_MESSAGE
+    commit_message: ( <<~COMMIT_MESSAGE
       (SIMP-7035) Update to new Travis CI pipeline
 
       This patch updates the Travis Pipeline to a static, standardized format
       that uses project variables for secrets. It includes an optional
-      diagnostic mode to test the project's variables against their respective
+      diagnostic mode to test the project\'s variables against their respective
       deployment APIs (GitHub and Puppet Forge).
 
-      SIMP-7035 #comment Update to latest pipeline in pupmod-simp-aide
-      SIMP-7624 #close
+      SIMP-7035 #comment Update to latest pipeline in pupmod-simp-at
+      SIMP-7617 #close
       COMMIT_MESSAGE
-    )
+    ),
+    approval_message: "Update of static, non-code file approved in https://github.com/simp/pupmod-simp-aide/pull/59",
+    merge_message: nil,
   }
-  repo_pr = forker.ensure_pr(upstream_reponame, opts )
+  upstream_reponame = 'simp/rubygem-simp-rake-helpers'
+  opts = {
+    target_branch: 'master',
+    fork_branch:   'SIMP-7707',
+    user: 'trevor-vaughan',
+    approval_message: ":+1: :ghost:",
+    approval_type: 'APPROVE', # APPROVE, REQUEST_CHANGES, or COMMENT. If left blank, the review is left PENDING.
+    merge_message: nil,
+  }
+  repo_pr = forker.approve_pr(upstream_reponame, opts )
+  repo_pr = forker.merge_pr(upstream_reponame, opts )
 
 require 'pry'; binding.pry
 end
---
+++ dist/puppetsync/tasks/approve_and_merge_github_pr.rb
@@ -0,0 +1,39 @@
+#!/opt/puppetlabs/bolt/bin/ruby
+# ------------------------------------------------------------------------------
+# Usage:
+#
+#   bolt task run puppetsync::ensure_github_fork \
+#      -t localhost \
+#      github_repo=simp/pupmod-simp-acpid
+#      github_user="$GITHUB_USER" \
+#      github_authtoken="$GITHUB_API_TOKEN" \
+#      extra_gem_paths='["/path/to/puppetsync/gems"]'
+#
+require_relative '../../ruby_task_helper/files/task_helper.rb'
+
+class MyTask < TaskHelper
+  def task(name: nil, **kwargs)
+    # Ensure that extra gem paths are loaded (to find octokit)
+    kwargs[:extra_gem_paths].each{ |gempath| Dir["#{gempath}/gems/*/lib"].each{ |path| $LOAD_PATH << path }}
+    require_relative '../../puppetsync/files/ensure_github_pr_forker.rb'
+    forker = GitHubPRForker.new( kwargs[:github_user], kwargs[:github_authtoken] )
+
+    opts = {
+      target_branch:  kwargs[:target_branch],
+      fork_branch:    kwargs[:fork_branch],
+      commit_message: kwargs[:commit_message],
+    }
+    repo_pr = forker.ensure_pr(kwargs[:target_repo], opts )
+    {
+      pr_created:       forker.created_pr,
+      pr_number:        repo_pr.number,
+      pr_url:           repo_pr.html_url,
+      upstream_repo:    repo_pr.base.repo.full_name,
+      upstream_branch:  repo_pr.base.ref,
+      user_repo:        repo_pr.head.repo.full_name,
+      user_branch:      repo_pr.head.ref,
+    }
+  end
+end
+
+MyTask.run if __FILE__ == $0
