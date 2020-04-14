8e914c1552e507eed64a199f3c365fa2e7adfec6
Chris Tessmer <chris.tessmer@onyxpoint.com>
2020-04-14 16:18:57 -0400

    rename common to base

---
+++ site-modules/profile/manifests/base.pp
@@ -0,0 +1,3 @@
+# Management common to EVERY repository (not just modules ― so be careful!)
+class profile::base {
+}
--- site-modules/profile/manifests/common.pp
+++
@@ -1,5 +0,0 @@
-# Management common to EVERY repository (not just modules) ― so be careful!
-class profile::common {
-  $org = $facts.dig('module_metadata','forge_org')
-  if $org { notify{"======== org: ${org}":} }
-}
