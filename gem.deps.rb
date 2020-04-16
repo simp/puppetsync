94031c2cba60841a3096d6124ffff45ff536643c
Chris Tessmer <chris.tessmer@onyxpoint.com>
2020-04-16 13:30:14 -0400

    Update links to Bolt's Ruby version

--- gem.deps.rb
+++ gem.deps.rb
@@ -1,8 +1,8 @@
 # See the following for reference:
-# - http://docs.ruby-lang.org/en/2.2.0/Gem/RequestSet/GemDependencyAPI.html
-# - http://docs.ruby-lang.org/en/2.2.0/Gem/RequestSet/GemDependencyAPI.html#method-i-group
-# - http://docs.ruby-lang.org/en/2.2.0/Gem/RequestSet/GemDependencyAPI.html#method-i-platform
-# - http://docs.ruby-lang.org/en/2.2.0/Gem.html#method-c-use_gemdeps
+# - http://docs.ruby-lang.org/en/2.5.0/Gem/RequestSet/GemDependencyAPI.html
+# - http://docs.ruby-lang.org/en/2.5.0/Gem/RequestSet/GemDependencyAPI.html#method-i-group
+# - http://docs.ruby-lang.org/en/2.5.0/Gem/RequestSet/GemDependencyAPI.html#method-i-platform
+# - http://docs.ruby-lang.org/en/2.5.0/Gem.html#method-c-use_gemdeps
 source 'https://rubygems.org'
 
 gem 'octokit', '~> 4.18'
