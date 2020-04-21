6981594fb4e63becfac57892891255b81857ba6c
Chris Tessmer <chris.tessmer@onyxpoint.com>
2020-04-21 15:21:01 -0400

    Allow per-module override for travis yml

--- site-modules/profile/manifests/pupmod/travis_yml.pp
+++ site-modules/profile/manifests/pupmod/travis_yml.pp
@@ -1,6 +1,17 @@
-# Static .travis.yml file for Puppet modules
+# Static .travis.yml file for Puppet modules.
+#
+# Specific modules can provide their own .travis.yml file using the filename
+# convention::
+#
+# @example:
+#
+#     files/pupmod/_travis.pupmod-simp-xxx.yaml
+#
 class profile::pupmod::travis_yml {
   file{ "${::repo_path}/.travis.yml":
-    content => file('profile/pupmod/_travis.yml'),
+    content => file(
+      "profile/pupmod/_travis.${facts['module_metadata']['name']}.yml",
+      'profile/pupmod/_travis.yml'
+    ),
   }
 }
