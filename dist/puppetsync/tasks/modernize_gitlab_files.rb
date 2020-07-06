e36c28084c377ffbbb7be297a2decd97f9fd2de5
Chris Tessmer <chris.tessmer@onyxpoint.com>
2020-07-06 15:32:19 -0400

    (SIMP-7855) Update gems to PE 2018.1.15 + 2019.8.1
    SIMP-7855 #comment update puppetsync pipelines to 2018.1.15

--- dist/puppetsync/tasks/modernize_gitlab_files.rb
+++ dist/puppetsync/tasks/modernize_gitlab_files.rb
@@ -2,25 +2,51 @@
 
 require 'fileutils'
 
-def split_gitlab_file(file)
-  project_root = File.dirname(file)
-  warn "=== file: #{file}"
-  content = File.read(file)
+def modernize_gitlab_ci(content)
   content.gsub!(%r{pup_5_5_16|pup_5_5_10}, 'pup_5_5_17')
   content.gsub!(%r{pup5\.5\.16|pup5\.5\.10}, 'pup5.5.17')
   content.gsub!(%r{pup_6_latest}, 'pup_6')
   content.gsub!(%r{pup_5_latest}, 'pup_5')
-  File.open(file, 'w') { |f| f.puts content }
+
+  # If there are no jobs for the new LTS, copy + gsub all the old LTS jobs
+  if content.scan( /^(pup6\.16\.10(?!-unit|-lint)[-a-z0-9]*):\s*$/m ).empty?
+    # Tested at: https://rubular.com/r/fuMdr0HDU1cqLd
+    old_lts_jobs = content.scan( /^(pup5\.5\.17(?!-unit|-lint)[-a-z0-9]*:.*?(?=\Z|^#|^pup))/m ).flatten
+    new_lts_jobs = old_lts_jobs.map{|x| x.gsub('pup_5_5_17','pup_6_16_0').gsub('pup5.5.17','pup6.16.0') }
+    new_content = "#{content}\n#{new_lts_jobs.join}"
+    return new_content unless new_lts_jobs.empty?
+    content
+  end
 end
 
-require 'json'
-stdin = STDIN.read
-params = JSON.parse(stdin)
-warn stdin
+if ARGF.filename == '-'
+  stdin = ''
+  puts "ARGF.file.lineno: '#{ARGF.file.lineno}'"
+  stdin = ARGF.file.read
+  require 'json'
+  warn "== stdin: '#{stdin}'"
+  params = JSON.parse(stdin)
+  file = params['file']
+else
+  file = ARGF.filename
+end
 
-file = params['file'] || ARGV
+warn "file: '#{file}'"
+content = File.read(file)
+
 raise('No .gitlab-ci.yml path given') unless file
 
-warn "\n\n#{file}"
-split_gitlab_file(file)
-warn "FINIS: #{__FILE__}"
+warn "\n== Modernizing Gitlab CI content"
+original_content = content.dup
+content = modernize_gitlab_ci(original_content)
+warn (content == original_content ? '  == content unchanged' : '  ++ content was changed!')
+
+File.open(file, 'w') { |f| f.puts content }
+
+warn "\n== Running a test YAML.load_file on #{file} to validate its syntax"
+
+require 'yaml'
+YAML.load_file(file)
+warn "  ++ Test YAML.load_file on #{file} succeeded!"
+
+warn "\n\nFINIS: #{__FILE__}"
--- site-modules/profile/manifests/pupmod/gitlab_ci.pp
+++ site-modules/profile/manifests/pupmod/gitlab_ci.pp
@@ -6,11 +6,49 @@
 #     - e.g., Cache, stages, YAML anchors, most jobs EXCEPT acceptance tests
 #     - This section should be static across all module repositories
 #
-#   2. Repo-specific CI jobs
-#     - acceptance tests and compliance tests
+#   2. Repo-specific CI content
+#     - Jobs that provide acceptance and compliance tests
+#     - These jobs vary from repo to repo, and are managed directly in
+#       repository
 #
+# This profile enforces the standardized content in `.gitlab-ci.yml`, but also
+# persists existing repo-specific content.  The repo-specific content must be
+# defined below the following two lines:
+#
+#  ```
+#  # Repo-specific content
+#  # ========================================================================
+#  ```
+#
+# @param existing_pipeline_content
+#   The content of the module's existing `.gitlab-ci.yml` file, used to detect
+#   and perist local changes.
+#
+#   * By default, this content read directly from the target's `$::repo_path` on
+#     the local filesystem.
+#   * If the target doesn't have a `.gitlab-ci.yml` file, default starter
+#     content will be sourced from this module.
+#
+#   Target-specific content will be detected by searching for a specific lines
+#   (see the profile's documentation for an example)
+#
+#   Anything below those lines will be saved, and added to Puppet-managed
+#   template.
 #
 class profile::pupmod::gitlab_ci {
+
+  # NOTE: as noted above, the default value first attempts to read in the
+  # target's existing `.gitlab-ci.yml`.  This allows us to persist
+  # locally-managed, repo-specific pipeline content while keeping the resource
+  # idempotent.
+  #
+  # In idiomatic Puppet, this content would need to be read on a remote
+  # target by a custom fact.  But--since we're running from the puppetsync Bolt
+  # plan--we know each target repo is on the same localhost filesystem as the
+  # Bolt compiler, and we can simply read it directly.
+  #
+  # The `file()` function only attempts to read content from the second
+  # (default) file if the target's `gitlab-ci.yaml` file doesn't exist.
   $existing_pipeline_content = file(
     "${::repo_path}/.gitlab-ci.yml",
     "${module_name}/pupmod/_gitlab-ci.blank_repo_section.yml"
@@ -21,14 +59,10 @@ class profile::pupmod::gitlab_ci {
   #    # Repo-specific content
   #    # ========================================================================
   #
-  # NOTE In idiomatic Puppet, this content would be provided by a fact.  But we
-  # assume we're on a localhost target, so we can do this.
-  #
-  # TODO simplify ithe regex after all modules are consistent with the template
+  # TODO simplify the regex after all modules are consistent with the template
   $pipeline_components = $existing_pipeline_content.split(
      /^# (?i:Repo-specific(?: pipeline)? content|Acceptance tests)\s*\n# *(?:=|-){40,}\s*$/
   )
-
   if $pipeline_components.count > 1 {
     $repo_specific_content = $pipeline_components[1,-1].join("\n")
   } else {
