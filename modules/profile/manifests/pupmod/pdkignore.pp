5517c6f312a43f474fde68821c0eaed79664b450
Chris Tessmer <chris.tessmer@onyxpoint.com>
2021-03-05 02:39:33 +0000

    (SIMP-9606) Switch from .pmtignore to .pdkignore
    [SIMP-9607] #close

---
+++ modules/profile/manifests/pupmod/pdkignore.pp
@@ -0,0 +1,20 @@
+# Static .pdkignore file for Puppet modules.
+#
+# Specific modules can provide their own .pdkignore file using the filename
+# convention::
+#
+# @example:
+#
+#     files/pupmod/_pdkignore.pupmod-simp-name
+#
+class profile::pupmod::pdkignore(
+  Stdlib::Absolutepath $target_pdkignore_path = "${::repo_path}/.pdkignore",
+  Optional[String[1]]  $target_module_name = $facts.dig('module_metadata','name'),
+){
+  file{ $target_pdkignore_path:
+    content => file(
+      "profile/pupmod/_pdkignore.${target_module_name}",
+      'profile/pupmod/_pdkignore'
+    ),
+  }
+}
