c7a02d0428397cadd958e2f5e3d465be30f258bb
Chris Tessmer <chris.tessmer@onyxpoint.com>
2021-06-10 22:45:44 -0400

    Use pdkignore instead of pmtignore

--- modules/role/manifests/pupmod.pp
+++ modules/role/manifests/pupmod.pp
@@ -6,7 +6,7 @@ class role::pupmod {
   include 'profile::pupmod::git_files'
   include 'profile::pupmod::puppet_lint'
   include 'profile::pupmod::rspec'
-  include 'profile::pupmod::pmtignore'
+  include 'profile::pupmod::pdkignore'
   include 'profile::github_actions'
   include 'profile::obsoletes'
 }
