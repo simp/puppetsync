6e6d6e835f77d7559814da1688c7d2c4684f0d72
Mike Riddle <michael.riddle@onyxpoint.com>
2023-07-24 16:20:34 -0400

    Added logic to exclude tpm from getting el 8 and higher os updates and added some safety logic to test-rocky8-steps.sh

--- scripts/test-rocky8-steps.sh
+++ scripts/test-rocky8-steps.sh
@@ -10,6 +10,8 @@ RUBY_EXE="${RUBY_EXE:-/opt/puppetlabs/bolt/bin/ruby}"
 
 "$BUNDLE_EXE" --path=../../.vendor/bundle
 rm -rf spec/fixtures/modules/pupmod-* || :
-"$BUNDLE_EXE" exec rake spec_prep && for i in spec/fixtures/modules/*; do SKIP_RAKE_TASKS=yes "$RUBY_EXE" ../../dist/puppetsync/tasks/modernize_metadata_json.rb "$i/metadata.json"; done
+"$BUNDLE_EXE" exec rake spec_prep && for i in spec/fixtures/modules/*; do 
+  test -f "$i/metadata.json" && SKIP_RAKE_TASKS=yes "$RUBY_EXE" ../../dist/puppetsync/tasks/modernize_metadata_json.rb "$i/metadata.json" || :
+done
 
 SPEC_OPTS="${SPEC_OPTS:---no-fail-fast}" "$BUNDLE_EXE" exec rake spec_standalone |& tee ../"$( jq .name metadata.json ).rspec.log" && rm -f Gemfile.lock
