7693a35fbce4441fbc1642c439f0efa6d06ccba8
Steven Pritchard <steve@sicura.us>
2023-10-23 12:06:36 -0500

    Fixes based on testing
    * Fix OS data glob
    * Exclude modules that do not already support EL8

--- dist/puppetsync/tasks/os_data.rb
+++ dist/puppetsync/tasks/os_data.rb
@@ -8,14 +8,14 @@ params = JSON.parse(stdin)
 
 Dir.chdir(params['path'])
 
-Dir.glob('data/**/os/OracleLinux*.yaml').each do |oracle_data|
+Dir.glob('data/**/OracleLinux*.yaml') do |oracle_data|
   next if %r{-[0-7]\.yaml$}.match?(oracle_data)
   rocky_data = oracle_data.sub('OracleLinux', 'Rocky')
   next if File.exist?(rocky_data)
   FileUtils.cp(oracle_data, rocky_data)
 end
 
-Dir.glob('data/**/os/Rocky*.yaml').each do |rocky_data|
+Dir.glob('data/**/Rocky*.yaml') do |rocky_data|
   alma_data = rocky_data.sub('Rocky', 'AlmaLinux')
   next if File.exist?(alma_data)
   FileUtils.cp(rocky_data, alma_data)
@@ -24,7 +24,7 @@ end
 # Add EL9 data anywhere we have EL8 data
 el = ['Rocky', 'AlmaLinux', 'CentOS', 'RedHat', 'OracleLinux']
 el.each do |supported_os|
-  Dir.glob("data/**/os/#{el}-8.yaml").each do |el8_data|
+  Dir.glob("data/**/#{supported_os}-8.yaml") do |el8_data|
     el9_data = el8_data.sub('8', '9')
     next if File.exist?(el9_data)
     FileUtils.cp(el8_data, el9_data)
