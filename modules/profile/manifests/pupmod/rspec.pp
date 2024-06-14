f1b7dbfbad6e42de7f14cc2af92723e6b5e2b873
Steven Pritchard <steve@sicura.us>
2024-06-14 11:44:22 -0500

    Manage spec/spec_helper.rb in Puppet modules

--- modules/profile/manifests/pupmod/rspec.pp
+++ modules/profile/manifests/pupmod/rspec.pp
@@ -1,12 +1,25 @@
-# Manages .rspec
-class profile::pupmod::rspec(
+# @summary Manages rspec
+# @param rspec_path Path to .rspec
+# @param spec_helper_path Path to spec_helper.rb
+# @param target_module_name Target module name
+class profile::pupmod::rspec (
+  # lint:ignore:top_scope_facts
   Stdlib::Absolutepath $rspec_path = "${::repo_path}/.rspec",
+  Stdlib::Absolutepath $spec_helper_path = "${::repo_path}/spec/spec_helper.rb",
+  # lint:endignore
   Optional[String[1]]  $target_module_name = $facts.dig('module_metadata','name'),
-){
-  file{ $rspec_path:
+) {
+  file { $rspec_path:
     content => file(
       "${module_name}/pupmod/_rspec.${target_module_name}",
       "${module_name}/pupmod/_rspec",
+    ),
+  }
+
+  file { $spec_helper_path:
+    content => file(
+      "${module_name}/pupmod/spec/spec_helper.${target_module_name}.rb",
+      "${module_name}/pupmod/spec/spec_helper.rb",
     ),
   }
 }
