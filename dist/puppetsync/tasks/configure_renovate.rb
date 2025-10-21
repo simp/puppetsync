4d705aed96d63297d4923fd4a38c0fe0cb4d4a72
Steven Pritchard <steve@sicura.us>
2025-10-21 15:41:52 -0500

    Additional gem updates

--- dist/puppetsync/tasks/configure_renovate.rb
+++ dist/puppetsync/tasks/configure_renovate.rb
@@ -27,10 +27,27 @@ File.write('renovate.json', "#{JSON.pretty_generate(config)}\n")
 
 # Update Gemfile to configure regex custom manager
 if File.exist?('Gemfile')
-  gemfile = File.read('Gemfile').lines
+  gemfile = File.read('Gemfile').lines(chomp: true)
   renovate_comment = '# renovate: datasource=rubygems versioning=ruby'
 
   updated_gemfile = gemfile.map.with_index do |line, index|
+    # Workarounds for test failures due to outdated dependencies
+    case line
+    when %r{^\s*gem\s+(['"])simp-beaker-helpers\1}
+      # Update simp-beaker-helpers dependency to ~> 2.0.0
+      line.sub!(%r{^(\s*gem\s).*$}, "\\1'simp-beaker-helpers', ENV.fetch('SIMP_BEAKER_HELPERS_VERSION', '~> 2.0.0')")
+    when %r{^\s*gem\s+(['"])simp-rake-helpers\1}
+      # Update simp-rake-helpers dependency to ~> 5.24.0
+      line.sub!(%r{^(\s*gem\s).*$}, "\\1'simp-rake-helpers', ENV.fetch('SIMP_RAKE_HELPERS_VERSION', '~> 5.24.0')")
+    when %r{\bsimp-rspec-puppet-facts\b}
+      # Update simp-rspec-puppet-facts dependency to ~> 4.0.0
+      line.sub!(%r{(?<![,|]\s\[)(['"])~>\s*[0-3](?:\.\d+){0,2}\1}, "'~> 4.0.0'")
+    when %r{\bpuppetlabs_spec_helper\b}
+      # Pin puppetlabs_spec_helper to ~> 8.0.0
+      line.sub!(%r{(?<![,|]\s\[)(['"])~>\s*[0-7](?:\.\d+){0,2}\1}, "'~> 8.0.0'")
+      line << ", '~> 8.0.0'" if %r{^\s*gem\s+(['"])puppetlabs_spec_helper\1$}.match?(line)
+    end
+
     last_line = index.positive? ? gemfile[index - 1] : nil
     line = if (m = %r{^(\s*)gem\b.*\bENV\b}.match(line)) && !last_line&.include?(renovate_comment)
              "#{m[1]}#{renovate_comment}\n#{line}"
@@ -38,14 +55,8 @@ if File.exist?('Gemfile')
              line.dup
            end
 
-    # Update simp-rake-helpers dependency to ~> 5.24.0
-    line.sub!(%r{(?<![,|]\s\[)(['"])(?:~>|>=)\s*\d+(?:\.\d+){0,2}\1}, "'~> 5.24.0'") if line.include?('simp-rake-helpers')
-
-    # Update simp-rspec-puppet-facts dependency to ~> 4.0.0
-    line.sub!(%r{(?<![,|]\s\[)(['"])~>\s*[0-3](?:\.\d+){0,2}\1}, "'~> 4.0.0'") if line.include?('simp-rspec-puppet-facts')
-
     line
   end
 
-  File.write('Gemfile', updated_gemfile.join)
+  File.write('Gemfile', "#{updated_gemfile.join("\n")}\n")
 end
